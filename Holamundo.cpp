48f84e500f53a4bc8d1aab1d81bac6c9d19fa808
Diana Zazueta <42899991+ZazuetaDiana@users.noreply.github.com>
2020-10-16 23:03:16 -0700

    Create Holamundo.cpp

---
+++ Holamundo.cpp
@@ -0,0 +1,37 @@
+ @@
+    @@ Instituto Tecnológico de Tijuana
+    @@ Depto de Sistemas y Computación
+    @@ En g. En Sistemas Computacionales
+    @@ Autor: Zazueta Lopez Diana  @ZazuetaDiana
+    @@ Repositorio:
+    @@ Fecha de revisión: 16 octubre del 2020
+    @@ 
+    @
+    @ Objetivo del programa:
+    @ Mostrar holamundo
+    @
+    @ Entradas, procesos y salidas:
+    @ Pulgar- 2 , ARM64, ...
+    @
+    @ Errores a reportar / retroalimentar
+    @ Para .....
+    @
+    @ 
+
+ograma en ambiente ( 32 bits) modo ARM, no funciona en AWS aarm64
+.sección  .texto
+.global _start
+.brazo
+
+// Pr
+_comienzo:
+    mov r0, # 1 @ STDOUT
+    adr r1, label @ R1 = dirección de la cadena (cadena)
+    mov r2, # 13 @ R2 = tamaño de la cadena
+    mov r7, # 4 @ R7 = número de llamada al sistema para 'escribir'
+    svc # 0 @ invocar syscall
+_salida:
+    mov r7, # 1 @ R7 = syscall para salir del sistema 'exit'
+    svc # 0 @ invocar syscall
+etiqueta:
+.string  "Hola Mundo \ n"
