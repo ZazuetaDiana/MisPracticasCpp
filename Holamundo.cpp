a571ad53ef8527d9b881632b53c7a097da19b72c
Diana Zazueta <42899991+ZazuetaDiana@users.noreply.github.com>
2020-10-23 02:22:38 -0700

    Update Holamundo.cpp

--- Holamundo.cpp
+++ Holamundo.cpp
@@ -1,16 +1,15 @@
- @@
-    @@ Instituto Tecnológico de Tijuana
-    @@ Depto de Sistemas y Computación
-    @@ En g. En Sistemas Computacionales
-    @@ Autor: Zazueta Lopez Diana  @ZazuetaDiana
-    @@ Repositorio:https://github.com/ZazuetaDiana/MisPracticasCpp/edit/main/Holamundo.cpp
-    @@ Fecha de revisión: 16 octubre del 2020
-    @@ 
-    @
-    @ Objetivo del programa:
-    @ Mostrar holamundo
-    @
-    
+ // Practica2.cpp
+// Instituto Tecnológico de Tijuana
+// Depto de Sistemas y Computación
+// Ing.En Sistemas Computacionales
+//
+// Autor: Zazueta Lopez Diana @ZazuetaDiana
+// Repositorio: https: //github.com/ZazuetaDiana/MisPracticasCpp/find/main
+// Fecha de revisión: 16 de octubre del 2020
+// 
+// Objetivo del programa:
+// Calcular la altura (h) para t segundos de un cuerpo lanzado hacia arriba a una velocidad inicial r:
+// h = rt - 16t ^ 2 en este ejemplo, t = 2 yr = 32.    
     # incluye  < iostream >
 usando el  espacio de nombres  std ;
 
